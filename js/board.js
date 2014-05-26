e927f167bc6006f157ad33270c4a5492bc722cab
Zeimyth <zeimyth@hotmail.com>
2014-05-25 21:51:39 -0600

    Board is now rendered in board.js instead of in index.html. Fixed css related to tiles with numbers being shifted down

--- js/board.js
+++ js/board.js
@@ -1,15 +1,17 @@
 goog.provide('zeimyth.Board');
 
+goog.require('goog.dom');
+
 /**
  * @constructor
  */
 zeimyth.Board = function() {
 	var dimensions = zeimyth.game.getDimensions();
 
+	this.domHelper = goog.dom.getDomHelper();
+
 	// Stores the current values in the game grid. Stored in column-major form.
 	this.grid = this.initializeGrid(dimensions);
-
-	this.render();
 };
 
 /**
@@ -37,7 +39,21 @@ zeimyth.Board.prototype.initializeGrid = function(dimensions) {
  * @private
  */
 zeimyth.Board.prototype.render = function() {
+	var outerDiv = this.domHelper.getElementByClass('gamegrid');
 
+	var dimensions = zeimyth.game.getDimensions();
+
+	for (var y = 0; y < dimensions.height; y++) {
+		var row = this.domHelper.createDom('div', 'row');
+
+		for (var x = 0; x < dimensions.width; x++) {
+			var tile = this.domHelper.createDom('div', this.grid[x][y] ? 'tile two' : 'tile empty', this.grid[x][y] ? this.domHelper.createTextNode('2') : null);
+
+			this.domHelper.appendChild(row, tile);
+		}
+
+		this.domHelper.appendChild(outerDiv, row);
+	}
 };
 
 zeimyth.Board.left = 0;
